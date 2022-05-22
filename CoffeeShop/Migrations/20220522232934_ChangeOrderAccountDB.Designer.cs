83ce1fa96c90ab9e93fd81059765151a0b2a4044
Clone <40709168+Cloneee@users.noreply.github.com>
2022-05-23 06:29:56 +0700

    Add Migration

--- CoffeeShop/Migrations/20220522232934_ChangeOrderAccountDB.Designer.cs
+++ CoffeeShop/Migrations/20220522232934_ChangeOrderAccountDB.Designer.cs
@@ -4,6 +4,7 @@ using CoffeeShop.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeShop.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220522232934_ChangeOrderAccountDB")]
+    partial class ChangeOrderAccountDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
